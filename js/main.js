19c13dab4cbb2556a310491db098a94b3869e0d3
Alisson <alisson.agp.rj@gmail.com>
2025-02-25 20:43:08 -0300

    pulei duas linhas

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var empresa;
     const calc = document.getElementById("calc");
     const campoNome = document.getElementById("campoNome");
     const campoAno = document.getElementById("campoAno");
+
     calc.addEventListener("click", () => {
         let p = new empresa.Pessoa();
         p.nome = campoNome.value;
@@ -11,5 +12,6 @@ var empresa;
         document.getElementById("nome").textContent = p.nome;
         document.getElementById("ano").textContent = p.anoNasc.toString();
         document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+        
     });
 })(empresa || (empresa = {}));
